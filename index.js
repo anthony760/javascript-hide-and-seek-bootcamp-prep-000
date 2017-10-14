afcfc3d8012d993e3e78b083ff0415ddc0228b4d
Anthony Lopez <anthony760@gmail.com>
2017-10-14 01:25:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,13 +19,5 @@ function deepestChild() {
   let current = document.querySelectorAll('div#grand-node');
   var lastChild;
 
-  if (current.length === 1) {
-    return current[0];
-  } else if (current.length > 1) {
-    lastChild = current[0];
-  } else {
-    return null;
-  }
 
-  current.pop();
 }
