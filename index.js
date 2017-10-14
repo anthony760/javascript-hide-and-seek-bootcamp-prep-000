de40cbc14f7aba9cdec711d806f56968a1f80cb6
Anthony Lopez <anthony760@gmail.com>
2017-10-14 02:18:40 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  var nested = document.getElementById('nested').querySelectorAll('div.target');
+  return nested[0];
+}
+
+function increaseRankBy(n) {
+  var ranks = document.getElementsByClassName('ranked-list');
+
+  for (let i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = ((i + parseInt(n)).toString())
+  }
+}
+
+function deepestChild() {
+  let current = document.querySelectorAll('div#grand-node *');
+  return current[current.length - 1];
+}
