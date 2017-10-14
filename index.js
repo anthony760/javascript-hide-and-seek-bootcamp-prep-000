30acb27473c68e1108979c2e1c87d7d3ddd28705
Anthony Lopez <anthony760@gmail.com>
2017-10-14 00:31:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  return document.getElementByClassName('target').querySelectorAll('div#nested');
+  return document.getElementsByClassName('target').querySelectorAll('div#nested');
 }
