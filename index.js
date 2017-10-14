91d1785b7e5622638a07cdbae390675a7c8e2b05
Anthony Lopez <anthony760@gmail.com>
2017-10-14 00:51:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ function nestedTarget() {
   var nested = document.getElementById('nested').querySelectorAll('div.target');
   return nested[0];
 }
+
+function increaseRankBy(n) {
+  var ranks = document.getElementsByClassName(ranked-list);
+
+  for (let i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = ((i + parseInt(n)).toString())
+  }
+}
